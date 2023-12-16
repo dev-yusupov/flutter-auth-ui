53d3bbb37b89dc8129d4390260a735c0562ce8ec
Bobur Yusupov <bobur.yu@anur.tech>
2023-12-16 16:14:11 +0500

    Deleted unwanted folders

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -2,6 +2,7 @@
 import 'package:authui/components/ButtonFilled.dart';
 import 'package:authui/components/ButtonUnfilled.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void hello() {
   print("Hello, World!");
@@ -12,31 +13,35 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
     return MaterialApp(
       home: Scaffold(
-        body: Stack(
-          fit: StackFit.expand,
-          children: [
-            Container(
+          body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
               decoration: const BoxDecoration(
-                image: DecorationImage(image: 
-                  AssetImage("assets/images/background.png"),
-                  fit: BoxFit.cover
-                ))
-              ),
-              const Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/background.png"),
+                      fit: BoxFit.cover))),
+          const Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     Image(image: AssetImage("assets/images/logo.png")),
                     SizedBox(height: 40.0),
-                    Button(title: "Login", onPressed: hello,),
+                    Button(
+                      title: "Login",
+                      onPressed: hello,
+                    ),
                     SizedBox(height: 20.0),
                     UnfilledButton(title: "Register", onPressed: hello),
-                    SizedBox(height: 20.0,),
+                    SizedBox(
+                      height: 20.0,
+                    ),
                     Text(
                       "Continue as a guest!",
                       style: TextStyle(
@@ -47,14 +52,12 @@ class WelcomePage extends StatelessWidget {
                         decorationStyle: TextDecorationStyle.solid,
                         height: 0,
                       ),
-                      )
+                    )
                   ],
                 ),
-                )
-              )
-          ],
-        )
-        ),
+              ))
+        ],
+      )),
     );
   }
 }
